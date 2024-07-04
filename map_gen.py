01568850ac219b5253efb99664be0d3246d29196
Peter Nagy <155898084+Skoteinos1@users.noreply.github.com>
2024-07-04 23:58:25 +0100

    Update map_gen.py

--- map_gen.py
+++ map_gen.py
@@ -6,3 +6,11 @@ import os
 cor_x = 255
 cor_y = -377
 game_name = 'vl4_'
+
+# pyautogui.moveTo(pos[0] + pos[1], pos[2] + pos[3])
+# print(pyautogui.position())
+
+x1 = 1658
+y1 = 48
+x2 = 1900
+y2 = 290
