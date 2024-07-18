af562ce3a7da3f897ed088f5871bd28298708825
Skoteinos1 <155898084+Skoteinos1@users.noreply.github.com>
2024-07-18 03:14:58 +0100

    Better guide

--- Minetest/map_gen.py
+++ Minetest/map_gen.py
@@ -6,15 +6,21 @@ import os
 
 # 1. Make empty GAME_NAME_.png image with desired dimensions... like 2000x2000.
 #   It can work fith larger maps, but you have to adjust few numbers down the line
-# 2. Make sure you are facing North and find out your position
-# 3. Make sure your map is not obstructed
-# 4. Run the code
-# 5. It will take screenshot, crop the image, remove pointers, paste it into your GAME_NAME_x.png image and make GAME_NAME_x+1.png
+# 2. Make sure you are facing North and find out your position in game
+# 3. Make sure your map is not obstructed by other window. We are doing screenshots here,
+# 4. Enter your location in variables cor_x and cor_y
+# 5. Run the code
+# 6. It will take screenshot, crop the image, remove pointers, paste it into your GAME_NAME_x.png image and make GAME_NAME_x+1.png
 # Enjoy
 
 cor_x = 255
 cor_y = -377
 game_name = 'GAME_NAME_'  # In case you play multiple maps
+
+# Length of side of map devided by 2.
+# For example if you want your map to be for coordinates between -1000 and +1000, create empty image with size of 2000x2000 pixels
+# and enter 1000 here
+side_of_map = 1000
 
 # pyautogui.moveTo(pos[0] + pos[1], pos[2] + pos[3])
 # print(pyautogui.position())
@@ -24,10 +30,6 @@ x1 = 1658
 y1 = 48
 x2 = 1900
 y2 = 290
-# Length of side of map devided by 2.
-# For example if you want your map to be for coordinates between -1000 and +1000, create empty image with size of 2000x2000 pixels
-# and enter 1000 here
-side_of_map = 1000
 
 # Screenshot / Open the input image as numpy array, convert to RGB
 img = pyautogui.screenshot()
@@ -55,7 +57,8 @@ npImage = np.dstack((npImage,npAlpha))
 # Extracting the image data & creating an numpy array out of it 
 img_arr = np.array(npImage)  # Somehow dstacked arrays ar not arrays
 
-# Transparent rectangles
+# Transparent rectangles. This removes your pointer on minimap as well as north pointer.
+# If they are not removed correctly. Edit numbers in next 2 lines.
 img_arr[108 : 134, 113 : 129] = (0, 0, 0, 0) 
 img_arr[0 : 11, 114 : 129] = (0, 0, 0, 0)
 
