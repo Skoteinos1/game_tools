59a40f7144560db99c89ddb1020b008ba88352cd
Skoteinos1 <155898084+Skoteinos1@users.noreply.github.com>
2025-06-24 17:19:04 +0100

    New file

---
+++ PoE1/EfficientTimeHubris.py
@@ -0,0 +1,16 @@
+# Most desired Jewels are really expensive.
+#
+# But what is the price of second best?
+# Or which is best for you from those cheapest 100?
+
+# Simply explained what this tool does:
+# 1. Get text from PoE trade site.
+# 2. Run code, which will scrap data from Timeless Jewel calculator
+# 3. We store data, so we don't have to scrap them again
+# 4. Get list of jewels which are cheap and best for you from that trade list.
+
+
+# Enter text from PoE trade:
+trade_text = """
+
+"""
